02612af3f136e0a0f1f6d609eac0fbf7264518a6
unknown <jpinzon95@hotmail.com>
2023-07-25 14:02:41 -0500

    Add domain service.

---
+++ src/main/java/com/dc/latam/domain/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.dc.latam.domain.service;
+
+import com.dc.latam.domain.DomainEmployee;
+import com.dc.latam.domain.repository.IEmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private IEmployeeRepository iEmployeeRepository;
+
+    public List<DomainEmployee> getAll() {
+        return this.iEmployeeRepository.getAll();
+    }
+    public Optional<DomainEmployee> getById(Long employeeId) {
+        return this.iEmployeeRepository.getById(employeeId);
+    }
+}
